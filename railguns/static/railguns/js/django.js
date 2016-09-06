a833882f6b1595badd07ef204fbdfeb7d8630677
NY <nypisces@msn.com>
2016-09-06 17:45:44 +0800

    增加f7的mainView

--- railguns/static/railguns/js/django.js
+++ railguns/static/railguns/js/django.js
@@ -15,10 +15,10 @@ $.ajaxSetup({
 });
 
 $(function() {
-//	$('body').on('click', 'a[rel*=external]', function(e) { // 打开外部链接，target=_blank不符合W3C标准
-//		window.open(this.href);
-//		e.preventDefault(); // 正确使用return false;
-//	});
+	$('body').on('click', 'a[rel*=external]', function(e) { // 打开外部链接，target=_blank不符合W3C标准
+		window.open(this.href);
+		e.preventDefault(); // 正确使用return false;
+	});
 	// 切换语言
 	$('#setlang > .item:not(.active)').one('click', function() {
 		$(this).prev().submit();
--- railguns/static/railguns/js/f7.js
+++ railguns/static/railguns/js/f7.js
@@ -3,3 +3,5 @@
 var myApp = new Framework7();
 
 var $$ = Dom7;
+
+var mainView = myApp.addView('.view-main');
