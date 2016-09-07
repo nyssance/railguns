3a4e4d5b1a3e937360c7122e6e00f7584a8a7de1
NY <nypisces@msn.com>
2016-09-07 11:56:36 +0800

    增加f7.js.map

--- railguns/static/railguns/js/libs.js
+++ railguns/static/railguns/js/libs.js
@@ -4,22 +4,16 @@ $(function() {
 	// Handlebars
 	var $content = $('#datacontent');
 	var endpoint = $content.data('endpoint');
-	if ((typeof endpoint) == 'string' && endpoint != '') {
-		$.ajax({
-			url : endpoint,
-			dataType : 'json', // 兼容IE
-			success : function(data) {
-				var source = $('#entry-template').html();
-				var template = Handlebars.compile(source);
-				var $newItems = $(template(data));
-				$content.html($newItems);
-				return false;
-			},
-			error : function(e) {
-				alert('获取数据失败，请刷新页面重试！');
-			}
-		});
-	}
+	$.ajax(endpoint).done(function(data) {
+		var source = $('#entry-template').html();
+		var template = Handlebars.compile(source);
+		var $newItems = $(template(data));
+		$content.html($newItems);
+		return false;
+	}).fail(function() {
+		alert('获取数据失败，请刷新页面重试！');
+	}).always(function() {
+	});
 	// jQuery Lazy
 	$('.lazy').Lazy({
 		effect : 'fadeIn',
