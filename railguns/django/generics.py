a5864ae32cb83058b84d2c6c8066a13395a24952
NY <nypisces@msn.com>
2016-09-14 00:27:14 +0800

    0.1.3

--- railguns/django/generics.py
+++ railguns/django/generics.py
@@ -1,24 +1,30 @@
 from django.conf import settings
 from django.shortcuts import render
+from django.utils.translation import ugettext_lazy as _
+from django.views.generic.base import TemplateView
 
 
-def list_view(request, name='', title=None, endpoint=''):
-    title = title if title else name
-    endpoint = endpoint if endpoint.strip() else '/{}{}'.format(settings.API_VERSION, request.get_full_path())
-    templates = {'mobile': '{}/{}.html'.format('mobile', name)} if name else {'mobile': 'railguns/f7/list.html'}
-    headers = get_headers(request, ['HTTP_APP_SCHEME', 'HTTP_USER_AGENT'])
-    return render(request, templates['mobile'], locals())
-
-
-def detail_view(request, *args, **kwargs):
-    name = kwargs.get('name', '')
-    title = kwargs.get('title', name)
-    endpoint = kwargs.get('endpoint', '/{}{}'.format(settings.API_VERSION, request.get_full_path()))
-    templates = {'mobile': '{}/{}.html'.format('mobile', name)} if name else {'mobile': 'railguns/f7/detail.html'}
-    headers = get_headers(request, ['HTTP_APP_SCHEME', 'HTTP_USER_AGENT', 'HTTP_HOST'])
-    print(headers)
-    return render(request, templates['mobile'], locals())
-
-
+# headers = get_headers(request, ['HTTP_APP_SCHEME', 'HTTP_USER_AGENT', 'HTTP_HOST'])
+# print(headers)
 def get_headers(request, keys=[]):
     return dict((key, value) for (key, value) in request.META.items() if key in keys)
+
+
+class BaseView(TemplateView):
+    name = None
+
+
+class WebView(BaseView):
+    def get(self, request, *args, **kwargs):
+        title = kwargs.get('title', '{} - {}'.format(_(self.name), _('app_name')))
+        endpoint = kwargs.get('endpoint', '/{}{}'.format(settings.API_VERSION, request.get_full_path()))
+        template_name = self.template_name if self.template_name else '{}.html'.format(self.name)
+        return render(request, template_name, locals())
+
+
+class MobileView(BaseView):
+    def get(self, request, *args, **kwargs):
+        title = kwargs.get('title', _(self.name))
+        endpoint = kwargs.get('endpoint', '/{}{}'.format(settings.API_VERSION, request.get_full_path().replace(kwargs.get('path', '/m/'), '/')))
+        template_name = self.template_name if self.template_name else 'mobile/{}.html'.format(self.name)
+        return render(request, template_name, locals())
