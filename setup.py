f66f35f2b6cf50156d5a110df1426d2a104ece88
NY <nypisces@msn.com>
2016-09-13 14:48:16 +0800

    0.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='railguns',
-    version='0.1.1',
+    version='0.1.2',
     install_requires=[
         'Django',
         'djangorestframework',
