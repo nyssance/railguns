ac97a844c9501637a56048ef2b420b483f2e9b66
NY <nypisces@msn.com>
2017-10-16 23:39:57 +0800

    更新到Django2.0

--- railguns/urls.py
+++ railguns/urls.py
@@ -1,8 +1,8 @@
 from django.apps.registry import apps
 from django.conf import settings
-from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import include, path, re_path
 from django.views.generic.base import RedirectView
 from django.views.i18n import JavaScriptCatalog
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
@@ -13,28 +13,27 @@ from .views import SwaggerSchemaView
 
 # 系统自带
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^jsi18n/$', JavaScriptCatalog.as_view(), name='javascript-catalog'),
-#     url(r'^jsi18n/(?P<packages>\S+?)/$', javascript_catalog)
+    path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog')  # https://docs.djangoproject.com/en/dev/topics/i18n/translation/#note-on-performance
 ]
 # 第三方
 urlpatterns += [
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^api-token-refresh/', refresh_jwt_token),
-    url(r'^api-token-verify/', verify_jwt_token),
-    url(r'^developer/documentation/', SwaggerSchemaView.as_view()),
-    # url(r'^search/', include('haystack.urls'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', obtain_jwt_token),
+    path('api-token-refresh/', refresh_jwt_token),
+    path('api-token-verify/', verify_jwt_token),
+    path('developer/documentation/', SwaggerSchemaView.as_view()),
+    # path('search/', include('haystack.urls'))
 ]
 # RailgunS
 urlpatterns += [
-    url(r'^download_url/(?P<pk>\w+)/$', download_url, name='download_url'),
-    url(r'^upload_params/(?P<pk>\w+)/$', upload_params, name='upload_params'),
-    url(r'^favicon\.ico', RedirectView.as_view(url='{}favicon.ico'.format(settings.STATIC_URL), permanent=True))
+    re_path(r'download_url/(?P<pk>\w+)/$', download_url, name='download_url'),
+    re_path(r'upload_params/(?P<pk>\w+)/$', upload_params, name='upload_params'),
+    path('favicon\.ico', RedirectView.as_view(url='{}favicon.ico'.format(settings.STATIC_URL), permanent=True))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if apps.is_installed('rosetta'):
-    urlpatterns += [url(r'^rosetta/', include('rosetta.urls'))]
+    urlpatterns += [path('rosetta/', include('rosetta.urls'))]
