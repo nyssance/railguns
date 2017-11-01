b7a27342add2bc50daa21f8440427d0943478766
NY <nypisces@msn.com>
2017-11-01 15:54:48 +0800

    增加listcreate和detailupdate类型

--- railguns/rest_framework/generics.py
+++ railguns/rest_framework/generics.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView, RetrieveAPIView, RetrieveUpdateAPIView
 
 from . import mixins
 
@@ -20,10 +20,31 @@ class ListAPI(mixins.ModelMixin, ListAPIView):
         return self.get_model().objects.filter(is_active=True)
 
 
+class ListCreateAPI(mixins.ModelMixin, ListCreateAPIView):
+    """列表 & 创建 API"""
+    def get_queryset(self):
+        if self.request.method == 'GET':
+            return self.get_model().objects.filter(is_active=True)
+        return self.get_model().objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user.id,
+                        username=self.request.user.username,
+                        user_image_uris=self.request.user.image_uris)
+
+
 class DetailAPI(mixins.ModelMixin, RetrieveAPIView):
     """详情 API"""
     def get_queryset(self):
         return self.get_model().objects.filter(is_active=True)
+
+
+class DetailUpdateAPI(mixins.ModelMixin, mixins.PutToPatchMixin, RetrieveUpdateAPIView):
+    """详情 & 更新 API"""
+    def get_queryset(self):
+        if self.request.method == 'GET':
+            return self.get_model().objects.filter(is_active=True)
+        return self.get_model().objects.all()
 
 
 class OwnerListAPI(mixins.ModelMixin, ListAPIView):
