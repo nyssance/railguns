39ffe58ffc1ac17666b36ef400c07182f6cedd2c
NY <nypisces@msn.com>
2017-11-26 13:09:42 +0800

    修正

--- railguns/django/generics.py
+++ railguns/django/generics.py
@@ -24,14 +24,14 @@ def generate_uri(urlstring, request):
 
 
 class WebView(TemplateView):
-    name = ''
-    title = ''
-    endpoint = ''
+    name = None
+    title = None
+    endpoint = None
 
     def get(self, request, *args, **kwargs):
         verbose_name = _(self.name.replace('y_list', 'ies').replace('_list', 's').replace('_detail', ''))
-        title = self.title if self.title else verbose_name
+        title = self.title if self.title is None else verbose_name  # 用 is None 才能传入空标题
         # '{} - {}'.format(verbose_name, _('app_name')) // TODO: PC版用这个
-        endpoint = self.endpoint if self.endpoint else '/{}{}'.format(settings.API_VERSION, request.get_full_path())
+        endpoint = self.endpoint if self.endpoint is None else '/{}{}'.format(settings.API_VERSION, request.get_full_path())
         template_name = self.template_name if self.template_name else '{}.html'.format(self.name)
         return render(request, template_name, locals())
