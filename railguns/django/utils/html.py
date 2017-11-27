cc92866b4c30e596913a241797fc9be2aa9a3846
NY <nypisces@msn.com>
2017-11-27 12:14:35 +0800

    优化html格式化

--- railguns/django/utils/html.py
+++ railguns/django/utils/html.py
@@ -2,9 +2,9 @@ from django.utils.html import format_html
 
 
 # django 自带的 format_html_join 方法非常鸡肋
-def format_html_field_join(field, format_string='{}'):
-    return format_html('<br>'.join(format_string.format(item) for item in field.all()))
+def format_html_field_join(field, format_string='{}', join_string='<br>'):
+    return format_html(join_string.join(format_string.format(item) for item in field.all()))
 
 
-def format_html_string_join(string):
-    return format_html('<br>'.join(string.split()))
+def format_html_string_join(string, format_string='{}', join_string='<br>'):
+    return format_html(join_string.join(format_string.format(item) for item in string.split()))
