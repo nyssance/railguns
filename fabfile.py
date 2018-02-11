f0f818202325e5c0b15c20dc54c21e35d89b3c18
NY <nyssance@icloud.com>
2018-02-12 01:28:50 +0800

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,9 +2,6 @@
 
 import os
 import shutil
-import sys
-
-from pip._vendor.distlib.compat import raw_input
 
 from fabric.colors import blue, cyan, green, magenta, red, yellow
 from fabric.decorators import task
@@ -17,6 +14,7 @@ from fabric.utils import puts
 # ===========
 env.project_name = os.path.basename(os.path.dirname(__file__))
 env.colorize_errors = True
+env.proxy = '127.0.0.1:1087'
 
 
 # ============
@@ -35,13 +33,13 @@ def hello():
 
 @task
 def update_project():
-    local('curl -fsSL https://raw.githubusercontent.com/nyssance/Free/master/gitignore/Python.gitignore > .gitignore')
+    curl('https://raw.githubusercontent.com/nyssance/Free/master/gitignore/Python.gitignore > .gitignore')
 
 
 @task
 def local_format():
     local('isort -rc --atomic .')
-    local('cd railguns && find . -iname "*.py" | xargs yapf -pi')
+    local('yapf -r -pi .')
 
 
 @task
@@ -54,70 +52,6 @@ def local_update_vendor():
     ]
     for filename in filenames:
         curl('https://unpkg.com/{0}@latest/dist/{1} > {2}/static/vendor/{1}'.format(filename.split('.')[0], filename, env.project_name))
-
-
-# =========
-# =  git  =
-# =========
-@task
-def commit_and_sync(comment=None):
-    """git commit and sync"""
-    output_list = local('git status', True).split('\n')
-    branch = output_list[0].replace('On branch ', '')
-    if branch in ['develop', 'master']:
-        puts('不允许在 {} 分支 用 {} 命令直接操作'.format(yellow(branch), get_function_name()))
-    elif 'nothing to commit' in output_list[-1]:
-        puts('{} 分支没有变动, 不需要提交'.format(yellow(branch)))
-        if 'is ahead of' in output_list[1]:
-            puts('同步 {} 分支'.format(yellow(branch)))
-            local('git push')
-    else:
-        local('git reset')
-        delete_files = [x.strip() for x in output_list if x.find('deleted:') != -1]
-        for file in delete_files:
-            filename = file.split(':')[1].strip()
-            local('git rm {}'.format(filename))
-        local('git add .')
-        if not comment:
-            comment = raw_input('请输入提交的注解: ')
-        local('git status')
-        local('git commit -m "{}"'.format(comment))
-        local('git push')
-
-
-@task
-def update_from_develop():
-    """从 develop 更新到当前分支"""
-    output_list = local('git status', True).split('\n')
-    branch = output_list[0].replace('On branch ', '')
-    if branch in ['develop', 'master']:
-        puts('不允许在 {} 分支 用 {} 命令直接操作'.format(yellow(branch), get_function_name()))
-    elif 'nothing to commit' in output_list[-1]:
-        local('git pull origin develop')
-    else:
-        local('git status')
-        puts('当前 {} 分支有更新未提交, 请先执行 fab git_commit 命令提交'.format(yellow(branch)))
-
-
-@task
-def update_to_develop():
-    """从当前分支更新到 develop """
-    output_list = local('git status', True).split('\n')
-    branch = output_list[0].replace('On branch ', '')
-    if branch in ['develop', 'master']:
-        puts('不允许在 {} 分支 用 {} 命令直接操作'.format(yellow(branch), get_function_name()))
-    elif 'nothing to commit' in output_list[-1]:
-        confirm = raw_input('是否已经update_from_develop? [y/N]: '.format(yellow(branch)))
-        if confirm.lower() in ['ok', 'y', 'yes']:
-            puts('从 {} 合并到 develop'.format(yellow(branch)))
-            local('git checkout develop')
-            local('git pull')
-            local('git merge {}'.format(branch))
-            local('git push')
-            local('git checkout {}'.format(branch))
-    else:
-        local('git status')
-        puts('当前 {} 分支有更新未提交, 请先执行 fab git_commit 命令提交'.format(yellow(branch)))
 
 
 # ========
@@ -134,10 +68,10 @@ def upload_to_pypi():
 # ============
 # = 工具方法  =
 # ============
+def curl(command=''):
+    local('curl -fsSL{} {}'.format(' -x {}'.format(env.proxy) if env.proxy else '', command))
+
+
 def safe_local_delete(path):
     if os.path.exists(path):
         shutil.rmtree(path)
-
-
-def get_function_name():
-    return sys._getframe(1).f_code.co_name  # _getframe()则是自己的名字
