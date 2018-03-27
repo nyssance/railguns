629dcb61044803b0457255ce7b7673b73804c755
NY <nyssance@icloud.com>
2018-03-27 21:26:51 +0800

    修正注释

--- railguns/django/db/models.py
+++ railguns/django/db/models.py
@@ -23,7 +23,7 @@ class BaseModel(DateTimeModelMixin):
 
 
 class OwnerModel(BaseModel):
-    user_id = models.PositiveIntegerField(default=0, editable=False) // TODO: default=1
+    user_id = models.PositiveIntegerField(default=0, editable=False) # TODO: default=1
     username = models.CharField(max_length=150, editable=False)  # 长度和Django的User保持一致
     user_images = models.CharField(_('images'), max_length=2000, blank=True, editable=False)
 
