f18cf8361ffaee1c7f92cfc768a6d0a9039f3198
NY <nyssance@icloud.com>
2018-08-10 23:23:03 +0800

    0.9.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='RailgunS',
-    version='0.9.5',
+    version='0.9.6',
     install_requires=[
         'Django',
         'django[argon2]',
