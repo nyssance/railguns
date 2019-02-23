8695db8d05b463a70af1aa675e711d0f713ff0c6
NY <nyssance@icloud.com>
2019-02-23 23:52:04 +0800

    0.10.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='RailgunS',
-    version='0.9.9',
+    version='0.10.0',
     install_requires=[
         'django[argon2]==2.2b1',
         'djangorestframework',
