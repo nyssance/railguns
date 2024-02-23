b43391dbf0ec08c442299a771d9ae01169bd06ee
NY <1937955+nyssance@users.noreply.github.com>
2024-02-24 04:47:23 +0800

    0.81

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='RailgunS',
-    version='0.80',
+    version='0.81',
     author='NY',
     author_email='nyssance@icloud.com',
     description='Only My Railgun',
@@ -24,17 +24,16 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Internet :: WWW/HTTP'
     ],
     # package_dir={'': 'src'},
     packages=find_packages(exclude=['tests']),
-    python_requires='>=3.10',
+    python_requires='>=3.12',
     # zip_safe=False,
     include_package_data=True,
     install_requires=[
-        'django[argon2]==5.0a1',
+        'django[argon2]==5.0.2',
         'djangorestframework==3.14.0',
         'djangorestframework_simplejwt==5.3.0',
         'gunicorn==21.2.0',
@@ -44,7 +43,7 @@ setup(
         'django-filter==23.2',
         'django-htmlmin==0.11.0',
         'hiredis==2.2.3',
-        'mysqlclient==2.1.1',
+        'mysqlclient==2.2.4',
         'redis==5.0.1'
     ],
     extras_require={
